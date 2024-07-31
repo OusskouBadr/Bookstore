b4e227ad9f1834f6892a58e433bf9722baf1102d
Badr OUSSKOU <ousskoubadr@gmail.com>
2024-07-31 10:33:49 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,3 +2,4 @@ export const PORT = 5555;
 
 export const mongoDBURL = 
 'mongodb+srv://root:root@books-store-mern.mpopdao.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+// Link to db on mgdb with unique key for api 
